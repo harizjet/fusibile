13abcab5afe731e967cf46050f03f88b8a5f18b6
Hariz Izzat <harizizzat09@gmail.com>
2023-01-24 05:04:39 +0800

    some changes

--- mathUtils.h
+++ mathUtils.h
@@ -9,17 +9,11 @@
 #endif
 #define M_PI_float    3.14159265358979323846f
 
-//rounding of positive float values
-#if defined(_WIN32)
-static float roundf ( float val ) {
-    return floor ( val+0.5f );
-};
-#endif
-
 /* get angle between two vectors in 3D
  * Input: v1,v2 - vectors
  * Output: angle in radian
  */
+ 
 static float getAngle ( Vec3f v1, Vec3f v2 ) {
     float angle = acosf ( v1.dot ( v2 ) );
     //if angle is not a number the dot product was 1 and thus the two vectors should be identical --> return 0
